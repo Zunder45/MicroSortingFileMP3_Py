39d3fbc80df682ce03ef340e8d873288df26b5f2
Zunder45 <sashaorlov02y@yandex.ru>
2021-07-23 23:31:38 +0500

    add -s and -u

--- Console_version(without_GUI)/AudoiFiles.py
+++ Console_version(without_GUI)/AudoiFiles.py
@@ -3,8 +3,6 @@ import Logg
 from colorama import Fore
 from logging import getLogger
 
-def exit():
-    sys.exit(1)
 
 
 class AudioFile():
@@ -18,25 +16,25 @@ class AudioFile():
 
 getLogger().setLevel('ERROR')
 
+audio_arr= []
 
-def sort(path,pathFrom):
-    audio_arr= []
+
+def scan(pathFrom, unknown = False):
     try:
-        os.chdir(path)
+        
+        if pathFrom[len(pathFrom)-1] != "/" :
+            pathFrom+="/"
+
+        os.chdir(pathFrom)
     except:
-        Logg.pr("Err 2","e")
-        exit()
+        Logg.pr("Err 1 (Ошибка пути)","e")
+        sys.exit(1)
         
     files = os.listdir(pathFrom)
+    Logg.pr("Поиск файлов в " + pathFrom)
     try:
-        
-        Logg.pr("Поиск файлов в " + pathFrom)
-
         for f in files:
-
             fp = pathFrom+"/"+f
-            
-
             if os.path.isdir(fp) != True and os.path.splitext(fp)[1] == ".mp3": # если это файл и имеет раширение .mp3
                 af = eyed3.load(fp)
                 if af.tag.artist != None: # если в фаеле указано имя артиста 
@@ -44,45 +42,47 @@ def sort(path,pathFrom):
                     audio_arr.append(auFl) # добавляем его в массив
                     Logg.pr(auFl.filePath+"   ["+auFl.artist+"]","o")
                 else:
-                    auFl = AudioFile("Неизвестный исполнитель", f, fp)
-                    audio_arr.append(auFl)
-                    Logg.pr(auFl.filePath+"   ["+auFl.artist+"]      !Артист не найлен!","a")
-
+                    if unknown:
+                        auFl = AudioFile("Неизвестный исполнитель", f, fp)
+                        audio_arr.append(auFl)
+                        Logg.pr(auFl.filePath+"   ["+auFl.artist+"]      !Артист не найден!","a")
+                    else:
+                        Logg.pr(fp+"     !Артист не найден! Пропускаем!","a")
         Logg.pr("\nКоличество файлов: " + str(len(audio_arr))) 
-            
-        if len(audio_arr) < 1:
-            exit()
+    except:
+        Logg.pr("Err 2 ","e")
+        sys.exit(1)
+    if len(audio_arr) < 1:
+        sys.exit(1)
 
-        
-        while(True):
-            Logg.pr("\nПродолжить?(д/н)") 
-            inp = input()
-            if inp == "" or inp == "д" or inp == "Д" or inp == "y" or inp == "Y":
-                break
-            elif  inp == "н" or inp == "Н" or inp == "n" or inp == "N":
-                exit()
-            else:
-                continue
 
-        Logg.pr("Создание каталогов и перемещение файлов...")
+def sort(path):
+    Logg.pr("Создание каталогов и перемещение файлов...")
+    try:
+        if path[len(path)-1] != "/" :
+            path+="/"
+        os.chdir(path)
+    except:
+        Logg.pr("Err 1 (Ошибка пути)","e")
+        sys.exit(1)
+    try:
         for i in audio_arr:
             name = i.fileName
             art = i.artist
             pathName = i.filePath
             try:
-                Logg.pr(pathName+"  {"+ path+art+"/"+name+"}")
                 os.replace(pathName,path+"/"+art+"/"+name)
                 Logg.pr(pathName+" ---> "+ art,"o")
             except(Exception):
                 try:
                     os.mkdir(path+"/"+art)
-                    Logg.pr("Создан каталог: "+pathName+"/"+art,"o")
+                    Logg.pr("Создан каталог: "+path+"/"+art,"o")
                     os.replace(pathName,path+"/"+art+"/"+name)
                     Logg.pr(pathName+" ---> "+ art,"o")
                 except(Exception):
                     Logg.pr(pathName+"    -x->   "+ art,"e")
 
 
-        Logg.pr(path,"o")
+        Logg.pr("Готово","o")
     except(Exception):
-        Logg.pr("err 1","e")
+        Logg.pr("err 3","e")
---
+++ Console_version(without_GUI)/Main.py
@@ -0,0 +1,38 @@
+import os, sys, argparse
+import AudoiFiles,Logg
+
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-p","--path", help="Путь к каталогу, где будут сортироватся файлы")# добавление аргументов
+parser.add_argument("-f",help="Каталог, из которого будут взяты файлы")
+parser.add_argument("-s",help="Пропускать 'Продолжить'",action="store_const", const="True")
+parser.add_argument("-u","--unknown",help="Брать файлы с неизвестными исполнителями",action="store_const", const="True")
+
+
+arg = parser.parse_args()
+
+if arg.path == None: # Если флаг -p пусть то 
+    path = os.getcwd() # Получение пути текущего каталога
+else:
+    path = arg.path
+if arg.f == None:
+    pathFrom = path
+else:
+    pathFrom = arg.f
+
+AudoiFiles.scan(pathFrom,arg.unknown)
+
+if not arg.s:
+    while(True):
+            Logg.pr("\nПродолжить?(д/н)") 
+            inp = input()
+            if inp == "" or inp == "д" or inp == "Д" or inp == "y" or inp == "Y":
+                break
+            elif  inp == "н" or inp == "Н" or inp == "n" or inp == "N":
+                sys.exit(1)
+            else:
+                continue
+
+AudoiFiles.sort(path)
